c7db47da798043f9c3d2c398aef26d0ff5470d3f
Jordan Duncan <jordan@dogfi.sh>
2016-10-15 12:46:48 +0100

    lol trying out raycasting

--- project/server.js
+++ project/server.js
@@ -8,7 +8,7 @@ var app = express();
 app.use(express.static('assets'));
 
 app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/pages/game.html');
+  res.sendFile(__dirname + '/pages/game3d.html');
 });
 
 app.listen(3000);
