5dde5e950363004a7baef06df6a6750029276c24
Scott Maxwell <scott@scotteh.me>
2016-10-15 16:56:29 +0100

    Add weapon name generator

--- project/chain.js
+++ project/chain.js
@@ -3,28 +3,50 @@
 var Foswig = require('foswig');
 
 var chain = new Foswig(3);
-function capitalise(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+
+function capword(s) {
+    return s.charAt(0).toUpperCase() + s.slice(1).toLowerCase();
 }
+function capitalise(s) {
+    if (s.split(' ').length > 1) {
+        return s.split(' ').map(capword).join(' ');
+    }
+    return capword(s);
+}
+
 var fs = require('fs');
 
-fs.readFile('census-dist-male-first.txt', 'utf8', function(err, data) {
+fs.readFile('./project/census-dist-male-first.txt', 'utf8', function(err, data) {
   if (err) throw err;
-  names = data.split('\n');
+  var names = data.split('\n');
   for (var name = 0; name < names.length - 1; name++) {
-    info = names[name].match(/\S+/g);;
+    var info = names[name].match(/\S+/g);;
     chain.addWordToChain(info[0]);
   }
 });
 
-fs.readFile('census-dist-female-first.txt', 'utf8', function(err, data) {
+fs.readFile('./project/census-dist-female-first.txt', 'utf8', function(err, data) {
   if (err) throw err;
-  names = data.split('\n');
+  var names = data.split('\n');
   for (var name = 0; name < names.length - 1; name++) {
-    info = names[name].match(/\S+/g);;
+    var info = names[name].match(/\S+/g);;
     chain.addWordToChain(info[0]);
+  }
+});
+
+var weapon_chain = new Foswig(2);
+fs.readFile('./project/weapons.txt', 'utf8', function(err, data) {
+  if (err) throw err;
+  var names = data.split('\n');
+  for (var name = 0; name < names.length - 1; name++) {
+    weapon_chain.addWordToChain(names[name].toUpperCase());
   }
 });
 
 exports.name_chain = chain;
-
+exports.gen_name = function() {
+    return capitalise(chain.generateWord(5, 10, false));
+};
+exports.gen_weapon = function() {
+    return capitalise(weapon_chain.generateWord(5, 10, false, 100));
+};
