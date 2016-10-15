4c42b21e2b564b11a0e3a318c29fb7f39271698f
Scott Maxwell <scott@scotteh.me>
2016-10-16 00:04:08 +0100

    Fix player killing bug on textual client, add mob killing

--- project/classes/Mob.js
+++ project/classes/Mob.js
@@ -1,6 +1,7 @@
 "use strict";
-import {Player} from "./Player";
 
+var Entity = require('./Entity.js').Entity;
+var utils = require('../utils.js');
 /*
  Mobs are entities not controlled by users
  and are hostile towards players
@@ -8,8 +9,8 @@ import {Player} from "./Player";
 
 const VIEW_DIST = 5;
 class Mob extends Entity {
-  constructor(name, y, x, h, dungeon, start_sword, start_armour, level) {
-    super(name, y, x, h, dungeon, start_sword, start_armour, level);
+  constructor(name, y, x, h, floor, start_sword, start_armour, level) {
+    super(name, y, x, h, floor, start_sword, start_armour, level);
     this.hunting = false;
   }
 
@@ -17,21 +18,21 @@ class Mob extends Entity {
     // Mob aimlessly wanders, searching for player
     var player;
     var directions = ['n', 'e', 'w', 's'];
-    var dir = Math.random() * (directions.length);
+    var dir = utils.randint(0, 3);
     dir = directions[dir];
     this.move(dir);
     // while this.hunting is false then idle
-    while(!this.hunting) {
+    // while(!this.hunting) {
       // search for players while idle
-      player = this.search();
-    }
-    this.hunt(player);
+      // player = this.search();
+    // }
+    // this.hunt(player);
   }
 
   search(){
     for(var m_y = this.y + VIEW_DIST; m_y >= this.y - VIEW_DIST; m_y--)
       for(var m_x = this.x - VIEW_DIST; m_x <= this.x + VIEW_DIST; m_x++)
-        if(this.dungeon.grid.occupied instanceof Player){this.hunting = true; return this.dungeon.grid.occupied;}
+        if(this.floor.grid.occupied instanceof Player){this.hunting = true; return this.floor.grid.occupied;}
   }
 
   hunt(player) {
@@ -44,6 +45,22 @@ class Mob extends Entity {
       if (Math.abs(player.y - this.y) > 5 || Math.abs(player.x - this.x) > 5) this.hunting = false;
     }
     this.idle();
+  }
+
+  erase() {
+    if (this.floor != null) {
+      this.floor.grid[this.y][this.x].occupied = false;
+      this.floor.npcs[this.uuid];
+      console.log(this.uuid + ' has perished');
+    }
+  }
+
+  die(){
+    var i = this.items;
+    // Drop any items the entity was carrying
+    if(i['sword'] != null) console.log('DROPPED ' + i['sword'])// this.dungeon.grid[this.y][this.x].item(sword)
+    // Erase entity
+    this.erase()
   }
 }
 
--- project/classes/Player.js
+++ project/classes/Player.js
@@ -1,11 +1,13 @@
 "use strict";
 var Entity = require('./Entity.js').Entity;
+var Global = require('../global.js');
 /*
  Player is a controllable entity
  */
 class Player extends Entity {
   constructor(name, y, x, h, floor, start_sword, start_armour, level) {
     super(name, y, x, h, floor, start_sword,  start_armour, level);
+    this.alive = true;
   }
 
   erase() {
@@ -20,6 +22,8 @@ class Player extends Entity {
   die() {
     super.die();
     this.erase();
+    this.alive = false;
+    Global.users[this.uuid].socket.sendUTF(JSON.stringify({"type": "death"}));
   }
 }
 
